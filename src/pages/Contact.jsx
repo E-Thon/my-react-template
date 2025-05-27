33b246b9347e2b2cb8fd76331da63fa5359df0d0
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-27 10:25:15 +0200

    contact formula added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router"
+import { Form, useNavigate } from "react-router"
 import { z } from "zod/v4";
 
 
 const contactSchema = z.object({
     name: z.string().min(1, "Name is required"), // "" besked er for optional fejlmeddelelese
     email: z.email().optional(), // optional = ikke et krav, kan hoppes over
-    textarea: z.string().min(10, "Message is too short"),
+    message: z.string().min(10, "Message is too short"),
 })
 
 export default function Contact() {
     const [errors, setErrors] = useState({})
-
+    const navigate = useNavigate();
+    
     function handleSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target); //alle data tages ud af formularen
@@ -23,22 +24,22 @@ export default function Contact() {
 
         if (!result.success) {
             const errors = z.treeifyError(result.error)
-            setErrors(errors.properties)
+            setErrors(errors.properties) // statevariabell
         } else {
             setErrors({}); // clearer fejlmeddeleleser efter submit
+
+            // her: fetch... method post:
+            fetch('https://jsonplaceholder.typicode.com/users', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(result.data)
+            })
+                .then(res => {
+                    if (res.ok) navigate("/")// evt til takkeside eller kvitteringsside (nu index)
+                })
         }
-
-
-        // her: fetch... method post:
-        fetch('https://jsonplaceholder.typicode.com/users', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(result.data)
-        })
-
-        redirect("/") // evt til takkeside eller kvitteringsside (nu index)
 
     }
     return (
